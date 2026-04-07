23a7acc80e08142086083194c892dbb8a175e10a
dhivakar07 <dhivakar782001@gmail.com>
2026-04-07 18:20:51 +0530

    add side navbar

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function addtocart(event){
-    event.target.textContent = "Added"
+function addtocart(event) {
+  event.target.textContent = "Added";
 }
+const sidenav = document.getElementById("sidenav");
+const close = document.getElementById("closenav");
+const menuicon = document.getElementById("menuicon");
+
+menuicon.addEventListener("click", function () {
+  sidenav.style.right = 0;
+});
+close.addEventListener("click", function () {
+  sidenav.style.right = "-30%";
+});
